c735ae229ce1daebb3abc07ab4b93780c1d0f938
junchen-liu <junchen_liu@126.com>
2022-02-05 18:13:51 -0500

    update zonemap

--- zonemaps.cpp
+++ zonemaps.cpp
@@ -10,12 +10,6 @@ zonemap<T>::zonemap(std::vector<T> _elements, uint _num_elements_per_zone)
 }
 
 template<typename T>
-bool zonemap<T>::query(T key)
-{
-    return false;
-}
-
-template<typename T>
 void zonemap<T>::build(){
     for (int i = 0; i < elements.size(); i++) {
         if (i % num_elements_per_zone == 0){
@@ -27,11 +21,43 @@ void zonemap<T>::build(){
             zones.push_back(zone);
         }
         zone<T> curr_zone = zones[i/num_elements_per_zone];
-        curr_zone.elements.push_back(elements[i])
+        curr_zone.elements.push_back(elements[i]);
         if(elements[i] > curr_zone.max){
             curr_zone.max = elements[i];
         } else if(elements[i] < curr_zone.min){
             curr_zone.min = elements[i];
         }
     }
+}
+
+template<typename T>
+bool zonemap<T>::query(T key){
+    for (int i = 0; i < zones.size(); i++){
+        if(key <= zones[i].max && key >= zones[i].min){
+            //Search for the elements
+            for(int j = 0; j < zones[i].elements.size(); j++){
+                if(zones[i].elements[j] == key){
+                    return true;
+                }
+            }
+        }
+    }
+    return false;//not find, return false
+}
+
+template<typename T>
+std::vector<T> zonemap<T>::query(T low, T high) {
+    std::vector<T> result;
+    for(int i = 0; i < zones.size(); i++) {
+        if(low > zones[i].max || high < zones[i].min){
+            continue;
+        }
+        for(int j = 0; j < zones[i].elements.size(); j++){
+            if(zones[i].elements[j] >= low && zones[i].elements[j] <= high){
+                result.push_back(zones[i].elements[j]);
+            }
+        }
+    }
+    return result;
+
 }
